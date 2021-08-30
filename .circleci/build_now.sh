d320ed446854631de1231d0b29208fd80e9501c1
Tomi Prasetio <tomy.prs25@gmail.com>
2021-08-30 17:44:27 +0700

    Builder: use proton

--- .circleci/build_now.sh
+++ .circleci/build_now.sh
@@ -154,7 +154,7 @@ DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%s")
 	if [ $COMPILER = "clang" ]
 	then
 		msg "|| Cloning Bionic Clang ||"
-		git clone --depth=1 https://github.com/bionicverse/bionic-clang clang-llvm
+		git clone --depth=1 https://github.com/kdrag0n/proton-clang clang-llvm
 		# Toolchain Directory defaults to clang-llvm
 		TC_DIR=$KERNEL_DIR/clang-llvm
 	fi
